0aec533b49132662fb8808d3b4775b225209c319
Rafael Caputo <rafaelcaputodev@gmail.com>
2021-05-05 10:42:48 -0300

    repository created

---
+++ src/main/java/com/caputo/composicao/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.caputo.composicao.repositories;
+
+import com.caputo.composicao.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
